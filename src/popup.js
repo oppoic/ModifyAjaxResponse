5b19fc5d6754372aeb16630ddf820bb235f182b2
wangjie <wangjie1@7net.cc>
2023-04-06 16:53:24 +0800

    commit

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,7 @@
-// let changeColor = document.getElementById("changeColor");
-// chrome.storage.sync.get("color", ({ color }) => {
-//   changeColor.style.backgroundColor = color;
-// });
+let changeColor = document.getElementById("changeColor");
+chrome.storage.sync.get("color", ({ color }) => {
+    changeColor.style.backgroundColor = color;
+});
 
 // 单击按钮时，将 setPageBackgroundColor 注入当前页面
 changeColor.addEventListener("click", async () => {
