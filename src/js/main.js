db571de2def0e8e05db3aec00c77731f0833d1dc
wangjie <wangjie1@7net.cc>
2023-04-13 14:22:59 +0800

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -12,8 +12,7 @@ function intercept_ajax(rules) {
         var callback = function () { };
         for (let i = 0, len = cacheRules.length; i < len; i++) {
             var curRule = cacheRules[i];
-            console.log('Request URL [' + method + ']' + url);
-            console.log('Match Rules [' + curRule.method + ']' + curRule.pattern);
+            console.log('[Request URL] ' + method + ':' + url + ' [Match Rule] ' + curRule.method + ':' + curRule.pattern);
             if ((curRule.method ? curRule.method === method : true) && curRule.pattern instanceof RegExp ? curRule.pattern.test(url) : (curRule.pattern === url || url.indexOf(curRule.pattern) > -1)) {
                 flag = true;
                 callback = curRule.callback;
@@ -26,7 +25,7 @@ function intercept_ajax(rules) {
                 var response = callback(event.target.responseText);
                 console.log('[Matched]');
                 console.log('Original Response ' + event.target.responseText);
-                console.log('Modify Response ' + response);
+                console.log('Modified Response ' + response);
                 Object.defineProperty(this, 'response', { writable: true });
                 Object.defineProperty(this, 'responseText', { writable: true });
                 this.response = this.responseText = response;
@@ -43,16 +42,16 @@ intercept_ajax([
         pattern: '/api/Common/UserInfo',//支持正则或完整url
         callback: function () {
             return JSON.stringify({
-                status: 200,
-                message: 'ok',
                 data: {
-                    isSuper: true,
-                    userGuid: '887cb663-40f1-4c5f-bd56-1e2ee094cbda',
                     userId: '18310787656',
                     userName: 'test',
-                    permissions: ['Agent'],
-                    ruInfo: { ru: '3408059', name: '学中溪龙', isUnion: false }
-                }
+                    userGuid: '887cb663-40f1-4c5f-bd56-1e2ee094cbda',
+                    isSuper: true,
+                    ruInfo: { ru: '3408059', name: '学中溪龙', isUnion: false },
+                    permissions: ['Agent']
+                },
+                status: 200,
+                message: 'ok'
             });
         }
     },
