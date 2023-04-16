6d15d0a81bdb23a3e858270e4a1f71f26937302c
wangjie <wangjie1@7net.cc>
2023-04-16 23:59:16 +0800

    commit

--- src/dist/myScript.js
+++ src/dist/myScript.js
@@ -1,5 +1,5 @@
-layui.use(['layer', 'form'], function () {
-    var layer = layui.layer, form = layui.form;
+layui.use(['layer', 'form', 'table'], function () {
+    var layer = layui.layer, form = layui.form, table = layui.table;
     //form.render();
 
     form.verify({
@@ -19,21 +19,46 @@ layui.use(['layer', 'form'], function () {
         }
     });
 
-    form.on('submit(add)', function () {
-        layer.msg('add');
-
-        return false;
+    table.render({
+        elem: '#configTable'
+        , cols: [[ //标题栏
+            { field: 'id', title: 'ID', width: 80, sort: true }
+            , { field: 'username', title: '用户名', width: 120 }
+            , { field: 'email', title: '邮箱', minWidth: 150 }
+            , { field: 'sign', title: '签名', minWidth: 160 }
+            , { field: 'sex', title: '性别', width: 80 }
+            , { field: 'city', title: '城市', width: 100 }
+            , { field: 'experience', title: '积分', width: 80, sort: true }
+        ]]
+        , data: [{
+            "id": "10001"
+            , "username": "杜甫"
+            , "email": "test@email.com"
+            , "sex": "男"
+            , "city": "浙江杭州"
+            , "sign": "人生恰似一场修行"
+            , "experience": "116"
+            , "ip": "192.168.0.8"
+            , "logins": "108"
+            , "joinTime": "2016-10-14"
+        }]
     });
-    form.on('submit(import)', function () {
-        layer.msg('import');
 
-        return false;
-    });
-    form.on('submit(export)', function () {
-        layer.msg('export');
+    // form.on('submit(add)', function () {
+    //     layer.msg('add');
 
-        return false;
-    });
+    //     return false;
+    // });
+    // form.on('submit(import)', function () {
+    //     layer.msg('import');
+
+    //     return false;
+    // });
+    // form.on('submit(export)', function () {
+    //     layer.msg('export');
+
+    //     return false;
+    // });
 
     form.on('submit(formConfig)', function (data) {
         layer.msg(JSON.stringify(data.field));
