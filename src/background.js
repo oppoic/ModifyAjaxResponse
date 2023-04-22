5d235d8ba92becc36b7f8c6d3fd04b33236adeb8
wangjie <wangjie1@7net.cc>
2023-04-22 18:18:51 +0800

    commit

--- src/background.js
+++ src/background.js
@@ -2,7 +2,7 @@ chrome.action.onClicked.addListener(() => {
     chrome.tabs.query({ url: chrome.runtime.getURL('config.html') }, function (tabs) {
         if (tabs.length > 0) {
             let tb = tabs[0];
-            chrome.windows.update(tb.windowId, { 'focused': true/*, 'drawAttention': true*/ }, function () {
+            chrome.windows.update(tb.windowId, { 'focused': true }, function () {
                 chrome.tabs.update(tb.id, { 'active': true });
             });
         }
--- src/dist/myScript.js
+++ src/dist/myScript.js
@@ -19,30 +19,30 @@ layui.use(['layer', 'form', 'table'], function () {
         }
     });
 
-    table.render({
-        elem: '#configTable'
-        , cols: [[ //标题栏
-            { field: 'id', title: 'ID', width: 80, sort: true }
-            , { field: 'username', title: '用户名', width: 120 }
-            , { field: 'email', title: '邮箱', minWidth: 150 }
-            , { field: 'sign', title: '签名', minWidth: 160 }
-            , { field: 'sex', title: '性别', width: 80 }
-            , { field: 'city', title: '城市', width: 100 }
-            , { field: 'experience', title: '积分', width: 80, sort: true }
-        ]]
-        , data: [{
-            "id": "10001"
-            , "username": "杜甫"
-            , "email": "test@email.com"
-            , "sex": "男"
-            , "city": "浙江杭州"
-            , "sign": "人生恰似一场修行"
-            , "experience": "116"
-            , "ip": "192.168.0.8"
-            , "logins": "108"
-            , "joinTime": "2016-10-14"
-        }]
-    });
+    // table.render({
+    //     elem: '#configTable'
+    //     , cols: [[ //标题栏
+    //         { field: 'id', title: 'ID', width: 80, sort: true }
+    //         , { field: 'username', title: '用户名', width: 120 }
+    //         , { field: 'email', title: '邮箱', minWidth: 150 }
+    //         , { field: 'sign', title: '签名', minWidth: 160 }
+    //         , { field: 'sex', title: '性别', width: 80 }
+    //         , { field: 'city', title: '城市', width: 100 }
+    //         , { field: 'experience', title: '积分', width: 80, sort: true }
+    //     ]]
+    //     , data: [{
+    //         "id": "10001"
+    //         , "username": "杜甫"
+    //         , "email": "test@email.com"
+    //         , "sex": "男"
+    //         , "city": "浙江杭州"
+    //         , "sign": "人生恰似一场修行"
+    //         , "experience": "116"
+    //         , "ip": "192.168.0.8"
+    //         , "logins": "108"
+    //         , "joinTime": "2016-10-14"
+    //     }]
+    // });
 
     // form.on('submit(add)', function () {
     //     layer.msg('add');
@@ -60,9 +60,9 @@ layui.use(['layer', 'form', 'table'], function () {
     //     return false;
     // });
 
-    form.on('submit(formConfig)', function (data) {
-        layer.msg(JSON.stringify(data.field));
+    // form.on('submit(formConfig)', function (data) {
+    //     layer.msg(JSON.stringify(data.field));
 
-        return false;
-    });
+    //     return false;
+    // });
 });
