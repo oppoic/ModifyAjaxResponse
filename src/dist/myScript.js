67d98beb16bbf442eb53127685151f901899f0ee
wangjie <wangjie1@7net.cc>
2023-04-26 23:47:30 +0800

    commit

--- src/dist/myScript.js
+++ src/dist/myScript.js
@@ -1,9 +1,27 @@
 
 $(function () {
-    $('#status').val('Enable');
-    $('#sort').val(1);
-    $('#method').val('get');
-    $('#pattern').val('/app\.json/gi');//转义？
-    $('#response').val('response');//转义？
+    // $('#status').val('Enable');
+    // $('#sort').val(1);
+    // $('#method').val('get');
+    // $('#pattern').val('/app\.json/gi');//转义？
+    // $('#response').val('response');//转义？
+
+
+    $('#iptStatus').on('click', function () {
+        if (($(this).prop('checked'))) {
+            $('#lblStatus').text('extension on');
+            $('#spnTitle').show();
+        }
+        else {
+            $('#lblStatus').text('extension off');
+            $('#spnTitle').hide();
+        }
+    });
+
+    $('#btnAdd').on('click', function () {
+        console.log('btnAdd click');
+
+    });
+
 
 });
