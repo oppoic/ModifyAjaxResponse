253b6ad0ee7e4fdc7f598f62918dc75511140887
wangjie <wangjie1@7net.cc>
2023-06-15 23:44:44 +0800

    commit

--- src/background.js
+++ src/background.js
@@ -1,5 +1,4 @@
 chrome.action.onClicked.addListener(() => {
-    console.log('background addListener1');
     chrome.tabs.query({ url: chrome.runtime.getURL('config.html') }, function (tabs) {
         if (tabs.length > 0) {
             let tb = tabs[0];
@@ -14,7 +13,6 @@ chrome.action.onClicked.addListener(() => {
 });
 
 chrome.storage.local.get(['on', 'onTime'], (result) => {
-    console.log('background get');
     if (result.hasOwnProperty('on') && result.on) {
         if (result.hasOwnProperty('onTime')) {
             var dtDiffer = parseInt(new Date(new Date().toLocaleString()) - new Date(result.onTime)) / 1000;//second
@@ -32,19 +30,7 @@ chrome.storage.local.get(['on', 'onTime'], (result) => {
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log('background addListener 2');
     if (request.type === 'modify_ajax_response_seticon') {
         chrome.action.setIcon({ path: "/images/16_gray.png" });
     }
-});
-
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-    console.log('background addListener 3');
-    // if (changes.hasOwnProperty('on'))
-    //     console.log(changes.on.newValue);
-    // if (changes.hasOwnProperty('onTime'))
-    //     console.log(changes.onTime.newValue);
-    // if (changes.hasOwnProperty('data'))
-    //     console.log(changes.data.newValue);
-
 });
--- src/content_scripts.js
+++ src/content_scripts.js
@@ -4,7 +4,6 @@ script.setAttribute('src', chrome.runtime.getURL('dist/pageInjectScript.js'));
 document.documentElement.appendChild(script);
 
 script.addEventListener('load', () => {
-    console.log('content_scripts addEventListener');
     chrome.storage.local.get(['on', 'onTime', 'data'], function (result) {
         if (result.hasOwnProperty('on') && result.on && result.hasOwnProperty('onTime') && result.hasOwnProperty('data') && result.data.length > 0) {
             var dtDiffer = parseInt(new Date(new Date().toLocaleString()) - new Date(result.onTime)) / 1000;//second
@@ -17,8 +16,15 @@ script.addEventListener('load', () => {
             }
         }
     });
+});
 
-    // setTimeout(() => {
-    //     postMessage({ type: 'modify_ajax_response_datachange' });
-    // }, 5000);
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    chrome.storage.local.get(['on', 'data'], function (result) {
+        if (changes.hasOwnProperty('on')) {
+            postMessage({ type: 'modify_ajax_response_datachange', on: changes.on.newValue, data: result.data });
+        }
+        if (changes.hasOwnProperty('data')) {
+            postMessage({ type: 'modify_ajax_response_datachange', on: result.on, data: changes.data.newValue });
+        }
+    });
 });
