1cd20ffc355dd35b2d16fc62bd2126d1c3ef57cf
wangjie <wangjie1@7net.cc>
2023-06-16 15:54:54 +0800

    commit

--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
 chrome.action.onClicked.addListener(() => {
     chrome.tabs.query({ url: chrome.runtime.getURL('config.html') }, function (tabs) {
         if (tabs.length > 0) {
-            let tb = tabs[0];
+            var tb = tabs[0];
             chrome.windows.update(tb.windowId, { 'focused': true }, function () {
                 chrome.tabs.update(tb.id, { 'active': true });
             });
@@ -17,10 +17,8 @@ chrome.storage.local.get(['on', 'onTime'], (result) => {
         if (result.hasOwnProperty('onTime')) {
             var dtDiffer = parseInt(new Date(new Date().toLocaleString()) - new Date(result.onTime)) / 1000;//second
             if (dtDiffer > 24 * 60 * 60) {
+                chrome.storage.local.set({ on: false });
                 chrome.action.setIcon({ path: "/images/16_gray.png" });
-                if (result.hasOwnProperty('on') && result.on) {
-                    chrome.storage.local.set({ on: false });
-                }
             }
         }
     }
