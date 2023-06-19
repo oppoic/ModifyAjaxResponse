f2cb74ba83f0351f19cbf8c3dac3cf858c882eab
wangjie <wangjie1@7net.cc>
2023-06-20 00:57:15 +0800

    commit

--- src/dist/pageInjectScript.js
+++ src/dist/pageInjectScript.js
@@ -6,26 +6,25 @@ function intercept_ajax(rules) {
         var url = open_arguments[1];
 
         var flag = false;
-        var resTxt = '';
+        var xhrTxt = '';
         for (let i = 0, len = rules.length; i < len; i++) {
             var curRule = rules[i];
-            // console.log('[Request URL] ' + method + ':' + url + ' [Match Rule] ' + curRule.method + ':' + curRule.pattern);
-            // if ((curRule.method ? curRule.method === method : true) && curRule.pattern instanceof RegExp ? curRule.pattern.test(url) : (curRule.pattern === url || url.indexOf(curRule.pattern) > -1)) {
-            if (method == curRule.method && url.indexOf(curRule.pattern) > -1) {
+            //console.log('[xhr] request:' + method + ' ' + url + ' pattern:' + curRule.method + ' ' + curRule.pattern);
+            if (method === curRule.method && url.indexOf(curRule.pattern) > -1) {
                 flag = true;
-                resTxt = curRule.response;
+                xhrTxt = curRule.response;
                 break;
             }
         }
 
         this.addEventListener('readystatechange', function (event) {
             if (flag && this.readyState === 4) {
-                // console.log('[Matched]');
-                // console.log('Original Response ' + event.target.responseText);
-                // console.log('Modified Response ' + response);
+                console.log('[xhr Matched]');
+                console.log('Original Response ' + event.target.responseText);
+                console.log('Modified Response ' + xhrTxt);
                 Object.defineProperty(this, 'response', { writable: true });
                 Object.defineProperty(this, 'responseText', { writable: true });
-                this.response = this.responseText = resTxt;
+                this.response = this.responseText = xhrTxt;
             }
         });
         return ajax_open_original.apply(this, open_arguments);
@@ -35,20 +34,26 @@ function intercept_ajax(rules) {
 const originFetch = fetch;
 function intercept_fetch(rules) {
     window.fetch = async (url, options) => {
+        //console.log(options.method);
+
         var flag = false;
+        var fetchTxt = '';
         for (let i = 0, len = rules.length; i < len; i++) {
             var curRule = rules[i];
-            // console.log('fetch request:' + curRule.pattern);
-            if (/*method == curRule.method &&*/ url.indexOf(curRule.pattern) > -1) {
+            //console.log('[fetch] request:' + url + ' pattern:' + curRule.pattern);
+            if (/*options.method.toLowerCase() === curRule.method &&*/ url.indexOf(curRule.pattern) > -1) {
                 flag = true;
+                fetchTxt = curRule.response;
                 break;
             }
         }
 
         const response = await originFetch(url, options);
         if (flag) {
-            // console.log('[[fetch matched]]');
-            return new Response(curRule.response, response);
+            console.log('[fetch Matched]');
+            console.log('Original Response ' + options.body);
+            console.log('Modified Response ' + fetchTxt);
+            return new Response(fetchTxt, response);
         }
         else {
             return response;
