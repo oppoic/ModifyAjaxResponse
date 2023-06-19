49c520944af3377c1c841d95604156142f2327fa
wangjie <wangjie1@7net.cc>
2023-06-19 21:17:04 +0800

    fetch intercept

--- src/dist/pageInjectScript.js
+++ src/dist/pageInjectScript.js
@@ -1,39 +1,60 @@
 var ajax_open_original = XMLHttpRequest.prototype.open;
-
 function intercept_ajax(rules) {
     XMLHttpRequest.prototype.open = function () {
         var open_arguments = arguments;
-        var method = open_arguments[0].toLowerCase(),
-            url = open_arguments[1];
+        var method = open_arguments[0].toLowerCase();
+        var url = open_arguments[1];
 
         var flag = false;
-        var callback = function () { };
+        var resTxt = '';
         for (let i = 0, len = rules.length; i < len; i++) {
             var curRule = rules[i];
             // console.log('[Request URL] ' + method + ':' + url + ' [Match Rule] ' + curRule.method + ':' + curRule.pattern);
             // if ((curRule.method ? curRule.method === method : true) && curRule.pattern instanceof RegExp ? curRule.pattern.test(url) : (curRule.pattern === url || url.indexOf(curRule.pattern) > -1)) {
             if (method == curRule.method && url.indexOf(curRule.pattern) > -1) {
                 flag = true;
-                callback = curRule.callback;
+                resTxt = curRule.response;
                 break;
             }
         }
 
         this.addEventListener('readystatechange', function (event) {
             if (flag && this.readyState === 4) {
-                var response = callback(event.target.responseText);
                 // console.log('[Matched]');
                 // console.log('Original Response ' + event.target.responseText);
                 // console.log('Modified Response ' + response);
                 Object.defineProperty(this, 'response', { writable: true });
                 Object.defineProperty(this, 'responseText', { writable: true });
-                this.response = this.responseText = response;
+                this.response = this.responseText = resTxt;
             }
         });
         return ajax_open_original.apply(this, open_arguments);
     };
 };
-window.intercept_ajax = intercept_ajax;
+
+const originFetch = fetch;
+function intercept_fetch(rules) {
+    window.fetch = async (url, options) => {
+        var flag = false;
+        for (let i = 0, len = rules.length; i < len; i++) {
+            var curRule = rules[i];
+            // console.log('fetch request:' + curRule.pattern);
+            if (/*method == curRule.method &&*/ url.indexOf(curRule.pattern) > -1) {
+                flag = true;
+                break;
+            }
+        }
+
+        const response = await originFetch(url, options);
+        if (flag) {
+            // console.log('[[fetch matched]]');
+            return new Response(curRule.response, response);
+        }
+        else {
+            return response;
+        }
+    };
+}
 
 window.addEventListener("message", function (event) {
     var dt = event.data;
@@ -46,6 +67,7 @@ window.addEventListener("message", function (event) {
         }
         else {
             intercept_ajax([]);
+            intercept_fetch([]);
         }
     }
 });
@@ -57,11 +79,10 @@ function change_rules(dt) {
             arrRules.push({
                 method: element.method,
                 pattern: element.pattern,
-                callback: function () {
-                    return element.response
-                }
+                response: element.response
             });
         }
     });
     intercept_ajax(arrRules);
+    intercept_fetch(arrRules);
 }
